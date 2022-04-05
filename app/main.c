3caafa4d56288888c754b912f29086008cec5a81
Shagulka <72942909+Shagulka@users.noreply.github.com>
2022-04-05 10:06:38 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -23,21 +23,18 @@ activate (GtkApplication *app,
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
-
-
-
-  
   
   grid = gtk_grid_new();
   gtk_grid_set_column_spacing(GTK_GRID(grid), 5);
   gtk_grid_set_row_spacing(GTK_GRID(grid), 5);
   gtk_widget_set_halign(grid, GTK_ALIGN_CENTER );
   gtk_grid_set_row_homogeneous(GTK_GRID(grid), 1);
- 
   gtk_window_set_child (GTK_WINDOW (window), grid);
+  
+  
   image = gtk_image_new_from_file("./image.bmp");
   gtk_grid_attach (GTK_GRID (grid), image, 0, 0, 8, 9);
-
+  
 
   button = gtk_button_new_with_label ("Горизонтальный укур");
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
@@ -66,6 +63,7 @@ activate (GtkApplication *app,
     button = gtk_button_new_with_label ("хз че");
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
   gtk_grid_attach (GTK_GRID (grid), button, 6, 9, 1, 1);
+  
 
   gtk_window_present (GTK_WINDOW (window));
 }
